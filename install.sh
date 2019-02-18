d0cd0a58f8c86d93c167c5a81f4e91394118dfea
Anton Nagorny <33011419+nagornik@users.noreply.github.com>
2019-02-18 16:38:43 +0800

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,50 +1,25 @@
-#!/bin/bash
-set -e
+#!/bin/sh
+#---------------------------------------------------------------- 
+# Shell Name：install
+# Description：Plug-in install script
+# Author：Starry
+# E-mail: starry@misstar.com
+# Time：2016-11-06 02:30 CST
+# Version: 1.6.11.07
+# Copyright © 2016 Misstar Tools. All rights reserved.
+#----------------------------------------------------------------*/
+clear
 
-WORKDIR="$(mktemp -d)"
-SERVERS=(114.114.114.114 114.114.115.115 180.76.76.76)
-# Not using best possible CDN pop: 1.2.4.8 210.2.4.8 223.5.5.5 223.6.6.6
-# Dirty cache: 119.29.29.29 182.254.116.116
-
-CONF_WITH_SERVERS=(accelerated-domains.china google.china apple.china)
-CONF_SIMPLE=(bogus-nxdomain.china)
-
-echo "Downloading latest configurations..."
-git clone --depth=1 https://git.coding.net/felixonmars/dnsmasq-china-list.git "$WORKDIR"
-#git clone --depth=1 https://pagure.io/dnsmasq-china-list.git "$WORKDIR"
-#git clone --depth=1 https://github.com/felixonmars/dnsmasq-china-list.git "$WORKDIR"
-#git clone --depth=1 https://bitbucket.org/felixonmars/dnsmasq-china-list.git "$WORKDIR"
-#git clone --depth=1 https://gitee.com/felixonmars/dnsmasq-china-list.git "$WORKDIR"
-#git clone --depth=1 https://gitlab.com/felixonmars/dnsmasq-china-list.git "$WORKDIR"
-#git clone --depth=1 https://code.aliyun.com/felixonmars/dnsmasq-china-list.git "$WORKDIR"
-#git clone --depth=1 http://repo.or.cz/dnsmasq-china-list.git "$WORKDIR"
-
-echo "Removing old configurations..."
-for _conf in "${CONF_WITH_SERVERS[@]}" "${CONF_SIMPLE[@]}"; do
-  rm -f /etc/dnsmasq.d/"$_conf"*.conf
-done
-
-echo "Installing new configurations..."
-for _conf in "${CONF_SIMPLE[@]}"; do
-  cp "$WORKDIR/$_conf.conf" "/etc/dnsmasq.d/$_conf.conf"
-done
-
-for _server in "${SERVERS[@]}"; do
-  for _conf in "${CONF_WITH_SERVERS[@]}"; do
-    cp "$WORKDIR/$_conf.conf" "/etc/dnsmasq.d/$_conf.$_server.conf"
-  done
-
-  sed -i "s|^\(server.*\)/[^/]*$|\1/$_server|" /etc/dnsmasq.d/*."$_server".conf
-done
-
-echo "Restarting dnsmasq service..."
-if hash systemctl 2>/dev/null; then
-  systemctl restart dnsmasq
-elif hash service 2>/dev/null; then
-  service dnsmasq restart
-else
-  echo "Now please restart dnsmasq since I don't know how to do it."
+## Check The Router Hardware Model 
+model=$(uname -m)
+cd /tmp/
+curl -s -k https://mirror1.misstar.com/download/$model/mtinstall -o /tmp/mtinstall
+if [ $? != 0 ];then
+    curl -s -k https://mirror2.misstar.com/download/$model/mtinstall -o /tmp/mtinstall
+    if [ $? != 0 ];then
+    	echo "下载程序失败,即将退出"
+        exit
+    fi
 fi
-
-echo "Cleaning up..."
-rm -r "$WORKDIR"
+chmod +x /tmp/mtinstall
+/tmp/mtinstall
